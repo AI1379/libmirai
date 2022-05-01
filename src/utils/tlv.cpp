031f8753f3b2e1a70d654c31c71a5dd55477b87b
劝退师 <ubuntu_e_a@126.com>
2022-05-01 17:39:26 +0800

    修改TLV

--- src/utils/tlv.cpp
+++ src/utils/tlv.cpp
@@ -1,8 +1,14 @@
 #include "mirai/utils/tlv.h"
+#include <fstream>
 
 namespace mirai::utils {
 
 Write& Write::writeU8() {
+
+    return *this;
+}
+
+Write& Write::writeU16() {
 
     return *this;
 }
