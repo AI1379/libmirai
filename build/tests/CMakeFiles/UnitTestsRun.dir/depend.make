d9a92f581568fec88f458565cd49bd2d93410736
AI1379 <listener1381@outlook.com>
2022-05-01 09:23:12 +0000

    删除文件 build

--- build/tests/CMakeFiles/UnitTestsRun.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for UnitTestsRun.
-# This may be replaced when dependencies are built.
--- build/tests/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/elsa/libmirai/tests/gtest/googlemock
-# Build directory: /home/elsa/libmirai/build/tests/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
